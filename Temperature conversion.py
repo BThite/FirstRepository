d63fdfeeb63402c3ba5b54c8384954146744489b
BThite <78474496+BThite@users.noreply.github.com>
2021-02-03 15:23:31 +0530

    Add files via upload

---
+++ Temperature conversion.py
@@ -0,0 +1,7 @@
+print("This is a program to convert temperature in celsius to Fahrenheit")
+
+def main():
+    celsius = eval(input("What is the celsius temperature?"))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees fahrenheit")
+main()
